60ac6c672a11166c5131bf3af72d5120194c2b02
hurrysG <harris.mahmood@yahoo.com>
2021-04-09 16:38:21 -0400

    Play and tutorial widgets implemented and page navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,89 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(home:MainScreenWidget()));
 }
+
+class MainScreenWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body:Container(
+            child: Column(
+              children:<Widget> [
+                Container(decoration: BoxDecoration(image: DecorationImage(image: AssetImage("images/honeycomb.png")))),
+                Center(child: Text("BUQ QUEST", style: TextStyle(fontSize: 45, color: Colors.amber))
+                ),
+                MenuWidget(),
+
+              ],)
+        )
+    );
+  }
+}
+
+class ChooseBugScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children:<Widget> [
+            Text("Choose your Bug")
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TutorialScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children:<Widget> [
+            Text("How to play:")
+          ],
+        ),
+      ),
+    );
+
+  }
+}
+class MenuWidget extends StatefulWidget {
+  @override
+  _MenuWidgetState createState() => _MenuWidgetState();
+}
+
+class _MenuWidgetState extends State<MenuWidget> {
+  @override
+  Widget build(BuildContext context) {
+    print("Hello");
+    return Container(
+      child: Column(
+        children: <Widget>[
+          RaisedButton(onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ChooseBugScreen())
+            );
+          }, child: Text("Play") , color: Colors.orange,),
+          RaisedButton(onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TutorialScreen())
+            );
+          },
+            child:Text("Tutorial"), color: Colors.orange)
+        ],
+      ),
+
+    );
+  }
+}
+
+
 
